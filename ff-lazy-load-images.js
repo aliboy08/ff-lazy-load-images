58eed52f9a1de9e77a70767392ade35f233a0646
aliboy08 <alistair.ponce@gmail.com>
2018-02-09 23:36:06 +0800

    added callback function

--- ff-lazy-load-images.js
+++ ff-lazy-load-images.js
@@ -1,11 +1,12 @@
-FFLazyLoadImages = function(){
+FFLazyLoadImages = function(callback){
 	
-	var images;
+	var images,
+		images_count;
 	
 	function _init(){
 		
 		images = document.querySelectorAll('img');
-		
+		images_count = images.length;
 		preventImagesLoad();
 		
 		// On document ready, load images
@@ -26,6 +27,11 @@ FFLazyLoadImages = function(){
 	}
 	
 	function loadImages(){
+		
+		if(typeof callback === 'function'){
+			console.log('have callback function');
+		}
+		
 		// Load Images
 		images.forEach(function(element, index){
 			var img_src = element.getAttribute('data-src');
@@ -33,6 +39,14 @@ FFLazyLoadImages = function(){
 			img.src = img_src;
 			img.onload = function() {
 				element.setAttribute('src', img_src);
+				images_count--;
+				
+				// On images load complete, run callback
+				if( images_count == 0 ) {
+					if(typeof callback === 'function'){
+						callback();
+					}
+				}
 			};
 		});
 	}
